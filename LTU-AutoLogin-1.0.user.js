53c5f69751b92f3c294d613c94604a2820989c39
amadeusolofsson <44613198+amadeusolofsson@users.noreply.github.com>
2024-09-26 04:39:34 +0200

    Update LTU-AutoLogin-1.0.user.js

--- LTU-AutoLogin-1.0.user.js
+++ LTU-AutoLogin-1.0.user.js
@@ -1,16 +1,40 @@
 // ==UserScript==
-// @name         LTU-AutoLogin
-// @description  Automatically fills and clicks the button to quickly log-in from the LTU weblogon site.
-// @version 1.0
-// @author sleepy
+// @name         LTU-AutoLogin (WebLogon and Canvas)
+// @description  Automatically logs in on both LTU WebLogon and Canvas sites
+// @author       Amadeus Olofsson
 // @match        https://weblogon.ltu.se/*
+// @match        https://idp.ltu.se/*
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    document.getElementById("username").value = "USERNAME_HERE";
-    document.getElementById("password").value = "PASSWORD_HERE";
+    const USERNAME = "Your username here";
+    const PASSWORD = "Your password here";
 
-    document.getElementsByClassName("btn-submit")[0].click();
+    // WebLogon
+    function autoLoginWebLogon() {
+        document.getElementById("username").value = USERNAME;
+        document.getElementById("password").value = PASSWORD;
+        document.getElementsByClassName("btn-submit")[0].click();
+    }
+
+    // Canvas
+    function autoLoginCanvas() {
+        document.getElementById('username').value = USERNAME;
+        document.getElementById('password').value = PASSWORD;
+        document.querySelector('button[name="_eventId_proceed"]').click();
+    }
+
+    // 
+    function runAutoLogin() {
+        if (window.location.hostname === "weblogon.ltu.se") {
+            autoLoginWebLogon();
+        } else if (window.location.hostname === "idp.ltu.se") {
+            autoLoginCanvas();
+        }
+    }
+
+    // Run the appropriate login function when the page is fully loaded
+    window.addEventListener('load', runAutoLogin);
 })();
